75401adfebc94b41cfc479b5c2524b7863be2762
Asir Hameem <56108077+asirhameem@users.noreply.github.com>
2020-09-06 17:48:38 +0600

    Database Modification

---
+++ database/db.php
@@ -0,0 +1,18 @@
+<?php
+
+	$host 	= "127.0.0.1";
+	$user = "root";
+	$pass	= "";
+	$name	= "online_food_order_system";
+
+	function dbConnection(){
+
+		global $host;
+		global $user;
+		global $pass;
+		global $name;
+
+		return $connection = mysqli_connect($host, $user, $pass, $name);
+	}
+
+?>
